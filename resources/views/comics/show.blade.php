8bee2df02f6b3c99d680fb72ecb2d90b1cc6bc6e
Maccio92 <valerio362@gmail.com>
2022-02-24 09:42:39 +0100

    add show css

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,23 +6,27 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row justify-content-center bg-dark">
-            <div class="col-3">
-                <h1 class="">{{ $comic->title }}</h1>
+        <div class="row bg-dark">
+            <div class="col">
+                <h1 class="text-light text-capitalize">{{ $comic->title }}</h1>
+            </div>
+        </div>
+        <div class="row bg-secondary">
+            <div class="col-12">
+                <h2 class="text-dark text-capitalize">autore: {{ $comic->author }}</h2>
+                <h3 class="text-dark text-capitalize">genere: {{  $comic->genre }}</h3>
             </div>
         </div>
         <div class="row">
             <div class="col">
-                <h2>{{ $comic->author }}</h2>
-                <h2>{{  $comic->genre }}</h2>
-            </div>
-            <div class="col">
-                <h3>{{ $comic->description }}</h3>
-            </div>
-            <div class="col">
-                <h2>{{  $comic->price }} €</h2>
+                <p>Descrizione:<br>{{ $comic->description }}</p>
             </div>
         </div>
+        <div class="row">
+            <div class="col">
+                <h5>Prezzo:{{  $comic->price }} €</h5>
+            </div>
+        </div>
+            <button class="btn btn-primary"><a class="text-light text-capitalize" href="{{ route('comics.index') }}">Back</a></button>
     </div>
-    <a href="{{ route('comics.index') }}">Back</a>
 @endsection
